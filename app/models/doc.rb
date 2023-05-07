c6447803fbbc1e8c52d55ad4577b6efe23f69311
seuraul <seuraul.0@gmail.com>
2023-05-07 16:50:02 -0300

    Association between Users and Docs

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,2 +1,3 @@
 class Doc < ApplicationRecord
+  belongs_to :user #, class_name: "user", foreign_key: "user_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_07_194026) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_07_194400) do
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
